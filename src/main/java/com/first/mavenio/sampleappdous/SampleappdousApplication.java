402c3f64ddaa898a5bc0242d76dc7c01aeeea93f
yashgo101 <125973466+yashgo101@users.noreply.github.com>
2023-02-22 14:57:15 +0530

    Add files via upload

---
+++ src/main/java/com/first/mavenio/sampleappdous/SampleappdousApplication.java
@@ -0,0 +1,13 @@
+package com.first.mavenio.sampleappdous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleappdousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleappdousApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,3 @@
+function myFunction(){
+    alert("Welcome ! the button is clicked");
+}
